fe5b9e0fb192dedb77c3cf0f08cf171fbc92e3e0
Uriel SA <fprueba337@gmail.com>
2026-09-11 05:36:34 -0600

    Create variables_datos.py

---
+++ variables_tipos_de_datos/variables_datos.py
@@ -0,0 +1,61 @@
+# estos son ejemplos para poner en practica mis conocimientos durante la carrera
+# estos son sintaxis, variables y tipos de datos basicos que todo programador debe saber cuando empieza
+# comencemos disfruta y analiza cada ejemplo suerte :)
+
+# primero que nada como podemos hacer un comentario con mucho texto completo y meter mas sin necesidad poner esto #
+
+"""
+Hola aqui podemos definir un comentario
+completo sin necesidad de poner el #
+"""
+
+'''
+Tambien podemos hacerlo asi
+no habra ningun problema 
+'''
+
+# variables y tipos de datos
+
+my_name = 'UrielSA' # str = string - texto
+universidad = 'HyBridge' # str = string - texto
+soy = 'Estudiante actual' # str = string - texto
+edad = 22 # int - numero entero
+altura = 1.80 # float - numero decimal
+soy_estudiante = True # bool - si es verdadero
+soy_estudiante2 = False # bool - si es falso
+
+'''
+Aqui mostrare unos ejemplos como podemos imprimir nuestro
+texto en la consola
+'''
+
+print(my_name) # se imprime el valor de la variable nombre
+print(universidad) # se imprime el valor de la variable universidad
+print(soy) # se imprime el valor de la variable soy
+print(edad) # se imprime el valor de la variable edad
+print(altura) # se imprime el valor de la variable altura
+print(soy_estudiante) # se imprime el valor de la variable soy_estudiante
+print(soy_estudiante2) # se imprime el valor de la variable soy_estudiante2
+
+# aqui podemos mostrar que tipo de dato esta utilizando cada variable
+
+print(type(my_name))
+print(type(universidad))
+print(type(soy))
+print(type(edad))
+print(type(altura))
+print(type(soy_estudiante))
+print(type(soy_estudiante2))
+
+# si quieres mas limpio el dato y no salga el mensaje class podemos hacerlo asi
+
+print(type(my_name).__name__)
+print(type(edad).__name__)
+print(type(altura).__name__)
+print(type(soy_estudiante).__name__)
+print(type(soy_estudiante2).__name__)
+
+# espero que te haya servido estos ejemplos de variables y datos en el lenguaje de python
+# no olvides seguirme y darle una estrellita ⭐ para seguir subiendo mas avances o ejemplos
+# los quiero recuerden comer frutas y verduras y bañense xd
+# 💙❤️
